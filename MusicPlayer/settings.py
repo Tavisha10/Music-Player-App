088c7538f5522c1a1113f20cc27f288174667841
Tavisha10 <Tavishapearl10@gmail.com>
2025-11-21 20:24:40 +0530

    Add deployment files (requirements, Procfile, settings)

--- MusicPlayer/settings.py
+++ MusicPlayer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,3 +125,4 @@ MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/'      # where to go after successful login
 LOGOUT_REDIRECT_URL = '/'     # where to go after logout
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
