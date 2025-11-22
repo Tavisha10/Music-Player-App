7c1bb5a3c5bda1e82185a365c29570cc2ceeb86d
Tavisha10 <Tavishapearl10@gmail.com>
2025-11-22 18:39:21 +0530

    update requirements

--- MusicPlayer/settings.py
+++ MusicPlayer/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 # =======================
 from pathlib import Path
 import os
+import dj_database_url
 from django.core.exceptions import ImproperlyConfigured
 import logging
 
@@ -144,12 +145,18 @@ WSGI_APPLICATION = 'MusicPlayer.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
+}
+'''{
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
